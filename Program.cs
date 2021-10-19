729a92596e159c12eb83e15b41ff0331a123d2fa
RubiTorresArmendariz <89751417+Fulanitos1208@users.noreply.github.com>
2021-10-19 12:32:06 -0500

    EjemploArreglo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AppConsole4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            celular[] a= new celular[3];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = new celular() ;
+                a[i].Nombre = "Rubi";
+                a[i].Lada = "+52";
+                a[i].Numero = "866-109-79-06";
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i].ToString());
+            }
+            {
+                
+                Console.ReadKey();
+            }
+           
+        }
+    }
+}
---
+++ celular.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConsole4
+{
+    class celular
+    {
+        protected string nombre;
+        protected string lada;
+        protected string numero;
+        public string Nombre
+        {
+            get
+            { 
+                return nombre;
+            }
+
+            set 
+            { 
+                nombre = value; 
+            }
+        }
+
+        public string Lada
+        {
+            get 
+            {
+                return lada;
+            }
+            set
+            {
+                lada = value;
+            }
+        }
+        
+
+        public string Numero
+        {
+            get
+            { 
+                return numero;
+            }
+            set 
+            {
+                numero = value; 
+            }
+        }
+        public celular()
+        {
+            nombre="";
+            lada ="";
+            numero = "";
+        }
+        public celular(string nombre, string lada, string  numero)
+        {
+            this.nombre = nombre;
+            this.lada = lada;
+            this.numero = numero;
+        }
+        
+        public override string ToString()
+        {
+            return  "Nombre: " + nombre +  "\n Lada y numero de telefono: " + lada +" "+  numero ;
+        }
+
+
+    }
+}
